491a84e519dae1d0c1f9fb2fa0b4d8dbbda54529
Olivia Cubela <oliviacubela@gmail.com>
2020-06-09 23:25:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   arr = [ ]
   sentence = "Hello there, and who are you?"
   sentence.split.map do |phrase|
-    arr << "#{phrase.reverse.}"
+    arr << "#{phrase.reverse}"
   end
   arr.join(" ")
 end
