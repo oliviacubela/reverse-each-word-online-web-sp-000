7f19077e076863241f1031ee47631dcd9062a4a1
Olivia Cubela <oliviacubela@gmail.com>
2020-06-09 23:21:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sentence = "Hello there, and who are you?"
-  sentence.split.each do |phrase|
-    puts phrase.join(" ")
+  sentence.split.map do |phrase|
+    puts "phrase.reverse"
   end
 end
